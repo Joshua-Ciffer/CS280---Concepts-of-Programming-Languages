7a72cca8e309e6f5955df64fd9843e6537c83ad3
Joshua Ciffer <Josh@Cyph-Desktop>
2020-09-30 00:08:00 -0400

    Completed HW 3

--- src/main/c++/RA 1/Recitation Assignment 1.cpp
+++ src/main/c++/RA 1/Recitation Assignment 1.cpp
@@ -10,73 +10,73 @@
 
 using namespace std;
 
-int main(int argc, char *argv[]) {
-	int numLines = 0, longestLine = 0, numWords = 0, numInts = 0;
-	if (argc > 1) {		// Program is run with arguments
-		ifstream fileInput(argv[1], ios::in);
-		if (fileInput.is_open()) {
-			cout << "File has been successfully opened: " << argv[1] << endl << endl;
-			while (!fileInput.eof()) {
-				string currentLine;
-				getline(fileInput, currentLine);
-				cout << currentLine << endl;
-				numLines++;
-				if (currentLine.length() > longestLine) {
-					longestLine = currentLine.length();
-				}
-				istringstream line(currentLine);
-				string word;
-				while (line >> word) {
-					numWords++;
-					bool numeric = true;
-					for (int i = 0; i < word.length(); i++) {
-						if (isdigit(word[i]) == false) {
-							numeric = false;
-							break;
-						}
-					}
-					if (numeric) {
-						numInts++;
-					}
-				}
-			}
-			fileInput.close();
-		} else {
-			cout << "File could not be opened: " << argv[1];
-			exit(1);
-		}
-	} else {	// No arguments given
-		cout << "No input file has been provided via program argument." << endl << endl;
-		do {
-			string currentLine;
-			cout << "Type: ";
-			getline(cin, currentLine);
-			numLines++;
-			if (currentLine.length() > longestLine) {
-				longestLine = currentLine.length();
-			}
-			istringstream line(currentLine);
-			string word;
-			while (line >> word) {
-				numWords++;
-				bool numeric = true;
-				for (int i = 0; i < word.length(); i++) {
-					if (isdigit(word[i]) == false) {
-						numeric = false;
-						break;
-					}
-				}
-				if (numeric) {
-					numInts++;
-				}
-			}
-		} while (!cin.eof());
-		numLines--;
-		cout << endl;
-	}
-	cout << endl << "Number of lines: " << numLines;
-	cout << endl << "Longest line: " << longestLine;
-	cout << endl << "Number of words: " << numWords;
-	cout << endl << "Number of integers: " << numInts;
-	return 0;
-}
+//int main(int argc, char *argv[]) {
+//	int numLines = 0, longestLine = 0, numWords = 0, numInts = 0;
+//	if (argc > 1) {		// Program is run with arguments
+//		ifstream fileInput(argv[1], ios::in);
+//		if (fileInput.is_open()) {
+//			cout << "File has been successfully opened: " << argv[1] << endl << endl;
+//			while (!fileInput.eof()) {
+//				string currentLine;
+//				getline(fileInput, currentLine);
+//				cout << currentLine << endl;
+//				numLines++;
+//				if (currentLine.length() > longestLine) {
+//					longestLine = currentLine.length();
+//				}
+//				istringstream line(currentLine);
+//				string word;
+//				while (line >> word) {
+//					numWords++;
+//					bool numeric = true;
+//					for (int i = 0; i < word.length(); i++) {
+//						if (isdigit(word[i]) == false) {
+//							numeric = false;
+//							break;
+//						}
+//					}
+//					if (numeric) {
+//						numInts++;
+//					}
+//				}
+//			}
+//			fileInput.close();
+//		} else {
+//			cout << "File could not be opened: " << argv[1];
+//			exit(1);
+//		}
+//	} else {	// No arguments given
+//		cout << "No input file has been provided via program argument." << endl << endl;
+//		do {
+//			string currentLine;
+//			cout << "Type: ";
+//			getline(cin, currentLine);
+//			numLines++;
+//			if (currentLine.length() > longestLine) {
+//				longestLine = currentLine.length();
+//			}
+//			istringstream line(currentLine);
+//			string word;
+//			while (line >> word) {
+//				numWords++;
+//				bool numeric = true;
+//				for (int i = 0; i < word.length(); i++) {
+//					if (isdigit(word[i]) == false) {
+//						numeric = false;
+//						break;
+//					}
+//				}
+//				if (numeric) {
+//					numInts++;
+//				}
+//			}
+//		} while (!cin.eof());
+//		numLines--;
+//		cout << endl;
+//	}
+//	cout << endl << "Number of lines: " << numLines;
+//	cout << endl << "Longest line: " << longestLine;
+//	cout << endl << "Number of words: " << numWords;
+//	cout << endl << "Number of integers: " << numInts;
+//	return 0;
+//}
---
+++ src/main/c++/RA 3/Recitation Assignment 3.cpp
@@ -0,0 +1,58 @@
+/*
+ * Recitation Assignment 3.cpp
+ *
+ *  Created on: Sep 29, 2020
+ *      Author: Joshua Ciffer
+ */
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <map>
+
+using namespace std;
+
+int main(int argc, char *argv[]) {
+	if (argc > 1) {
+		ifstream fileInput(argv[1], ios::in);
+		if (fileInput.is_open()) {
+			cout << "File has been successfully opened: " << argv[1] << endl << endl;
+			map<string, int> wordCount;
+			while (!fileInput.eof()) {
+				string currentLine, word;
+				getline(fileInput, currentLine);
+				istringstream wordParser(currentLine);
+				while (wordParser >> word) {
+					if (wordCount.find(word) == wordCount.end()) {
+						wordCount[word] = 1;
+					} else {
+						wordCount[word]++;
+					}
+				}
+			}
+			string mostFrequentWord = wordCount.begin()->first;
+			int numMostFrequentWord = wordCount.begin()->second;
+			map<string, int>::iterator it;
+			for (it = wordCount.begin(); it != wordCount.end(); it++) {
+				if (it->second > numMostFrequentWord) {
+					mostFrequentWord = it->first;
+					numMostFrequentWord = it->second;
+				}
+			}
+			cout << "Word with highest number of occurrences:" << endl;
+			cout << mostFrequentWord << " - " << numMostFrequentWord << endl << endl;
+			cout << "List of words in file and number of occurrences:" << endl;
+			for (it = wordCount.begin(); it != wordCount.end(); it++) {
+				cout << it->first << " - " << it->second << endl;
+			}
+			fileInput.close();
+			return 0;
+		} else {
+			cout << "File could not be opened: " << argv[1];
+			fileInput.close();
+			return 1;
+		}
+	} else {
+		cout << "No file provided via program argument.";
+		return 1;
+	}
+}
